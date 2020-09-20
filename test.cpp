c63c1dd89739d04129bcd137c02c64c65007884f
StarryFK <starryfk@hotmail.com>
2020-09-20 20:41:06 +0800

    [mod] update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <ctime>
+#define ull unsigned long long 
 using namespace std;
 
-int main(){
-    unsigned long long a = 1;
-    for(int i=0; i<40; i++){
-        cout << i << ':' << a%37 << endl;
-        a <<= 1;
-    }
-    cout << endl;
+int ha[36];
 
+int main(){
+    ios::sync_with_stdio(false);
+    double t1 = (double)clock()/CLOCKS_PER_SEC;
+    ull n=1;
+    for(int i=0; i<36; i++){
+        ha[n%37]=i;
+        n<<=1;
+    }
+    while(cin>>n){
+        cout << n << ':';
+        while(n){
+            cout << ha[(n&(-n)) % 37] << ' ';
+            n -= n&(-n);
+        }
+        cout << endl;
+    }
+    double t2 = (double)clock()/CLOCKS_PER_SEC;
+    cout << t2-t1 << 's' << endl;
 }
