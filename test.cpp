62a3fd3994576027972b2b1bb5f19a7105de873c
StarryFK <starryfk@hotmail.com>
2020-09-22 21:56:32 +0800

    [mod] add out.txt to ignore list and change test.cpp

--- test.cpp
+++ test.cpp
@@ -1,26 +1,23 @@
+#include <cstdio>
 #include <iostream>
 #include <ctime>
 #define ull unsigned long long 
 using namespace std;
 
-int ha[36];
 
 int main(){
     ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     double t1 = (double)clock()/CLOCKS_PER_SEC;
-    ull n=1;
-    for(int i=0; i<36; i++){
-        ha[n%37]=i;
-        n<<=1;
-    }
-    while(cin>>n){
-        cout << n << ':';
-        while(n){
-            cout << ha[(n&(-n)) % 37] << ' ';
-            n -= n&(-n);
-        }
-        cout << endl;
+    double num;
+    while(cin>>num);
+//    while(scanf("%lf", num)!=EOF);
+    for(int i=1; i<10000000; i++){
+        cout << i << '\n';
+//        printf("%d\n", i);
     }
     double t2 = (double)clock()/CLOCKS_PER_SEC;
-    cout << t2-t1 << 's' << endl;
+    cout << t2-t1 << '\n';
+//    printf("%f\n", t2-t1);
 }
