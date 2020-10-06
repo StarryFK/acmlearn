d23bb7f8a14068ccc35173f0ca220cf9a5a01710
StarryFK_304 <starryfk@hotmail.com>
2020-10-06 22:14:36 +0800

    [add] poj2018

--- test.cpp
+++ test.cpp
@@ -6,18 +6,8 @@ using namespace std;
 
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    double t1 = (double)clock()/CLOCKS_PER_SEC;
-    double num;
-    while(cin>>num);
-//    while(scanf("%lf", num)!=EOF);
-    for(int i=1; i<10000000; i++){
-        cout << i << '\n';
-//        printf("%d\n", i);
-    }
-    double t2 = (double)clock()/CLOCKS_PER_SEC;
-    cout << t2-t1 << '\n';
-//    printf("%f\n", t2-t1);
+    int a=0x7FFFFFFF;
+    cout << a << endl;
+    a>>=1;
+    cout << a << endl;
 }
