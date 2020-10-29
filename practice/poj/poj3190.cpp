16d6cb59da798d6f9f6f73ad7ac905221a5593bd
StarryFK <starryfk@hotmail.com>
2020-10-29 22:49:20 +0800

    [mod] coding poj3190.cpp

--- practice/poj/poj3190.cpp
+++ practice/poj/poj3190.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <cstring>
 #include <cstdio>
 #include <cstring>
 #include <queue>
 #include <functional>
 #include <algorithm>
-#define scanf("%d", &x)
+#define RI(x) scanf("%d", &x)
 using namespace std;
 
-priority_queue<int, vector<int>, greater<int> > q;
 
 struct Cow{
 	int in;
@@ -24,19 +24,62 @@ bool operator<(const Cow &a, const Cow &b){
 
 vector<Cow> v;
 
+struct Stall{
+	int out;
+	int index;
+	Stall(int a, int b){
+		out = a;
+		index = b;
+	}
+};
+
+priority_queue<Stall> q;
+
+bool operator<(const Stall &a, const Stall &b){
+	return a.out > b.out;
+}
+
+void clearq(priority_queue<Stall> &obj){
+	priority_queue<Stall> empty;
+	swap(empty, obj);
+}
+
+int cis[50005];
+
 int main(){
 	int n;
 	while(~RI(n)){
+		memset(cis,0,sizeof(cis));
 		int l,r;
+		clearq(q);
 		for(int i=0; i<n; i++){
 			RI(l);
 			RI(r);
-			v.push_back(l,r);
+			v.push_back(Cow(l,r));
 		}
 		sort(v.begin(), v.end());
-		for(auto i : v){
+		int scnt=0;
+		int ccnt=0;
+		for(int i=0; i<(int)v.size(); i++){
 
+			if(q.empty()){
+				q.push(Stall(v[i].out, ++scnt));
+				cis[ccnt++] = scnt; 
+			}else{
+				if(q.top().out<=v[i].in){
+					int index=q.top().index;
+					q.pop();
+					q.push(Stall(v[i].out, index));
+					cis[ccnt++] = index;
+				}else{
+					q.push(Stall(v[i].out, ++scnt));
+					cis[ccnt++] = scnt;
+				}
+			}
 		}
-		printf("%d\n", maxn);
+		printf("%d\n", scnt);
+		for(int i=0; i<ccnt; i++){
+			printf("%d\n", cis[i]);
+		}
 	}
 }
