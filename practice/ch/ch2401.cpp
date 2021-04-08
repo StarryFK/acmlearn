ec58878336535fd77b7ea7fc774673ddb9e68c15
StarryFK <starryfk@hotmail.com>
2021-04-08 21:26:35 +0800

    [mod] finish ch2401 "送礼物"

--- practice/ch/ch2401.cpp
+++ practice/ch/ch2401.cpp
@@ -9,16 +9,16 @@ using namespace std;
 #define RLL(x) scanf("%lld", &x)
 
 LL arr[50];
-vector<LL> pw;
-set<LL> pws;
+LL pws[1<<23], pw[1<<23];
+LL tos=0, tow=0;
 LL w,n;
-LL ans = 0;
+LL ans=0;
 
-void dfs1(int cur, LL dst, LL tot){
+void dfs1(LL cur, LL dst, LL tot){
 	if(tot>w){
 		return;
 	}
-	pws.insert(tot);
+	pws[tos++] = tot;
 	if(cur>dst){
 		return;
 	}
@@ -26,11 +26,11 @@ void dfs1(int cur, LL dst, LL tot){
 	dfs1(cur+1, dst, tot+arr[cur]);
 }
 
-void dfs2(int cur, LL dst, LL tot){
+void dfs2(LL cur, LL dst, LL tot){
 	if(tot>w){
 		return;
 	}
-	ans = max(ans, tot + *(upper_bound(pw.begin(), pw.end(), w-tot)-1));
+	ans = max(ans, tot + *(upper_bound(pw, pw+tow, w-tot)-1));
 	if(cur>dst){
 		return;
 	}
@@ -45,10 +45,13 @@ int main(){
 		RLL(arr[i]);
 	}
 	sort(arr, arr+n, greater<int>());
-	dfs1(0, n/2, 0);
-	for(auto i : pws){
-		pw.push_back(i);
+	dfs1(0, n/2-1, 0);
+	sort(pws, pws+tos);
+	for(LL i=0; i<tos; i++){
+		if(i>0 && pws[i]!=pws[i-1]){
+			pw[tow++] = pws[i];
+		}
 	}
-	dfs2(n/2+1, n-1, 0);
+	dfs2(n/2, n-1, 0);
 	printf("%lld\n", ans);
 }
